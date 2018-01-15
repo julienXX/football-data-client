e340f212ddac8eac8b47e0da078e6f6105691219
Julien Blanchard <julien@sideburns.eu>
2018-01-15 19:03:04 +0100

    WIP

--- src/Client.hs
+++ src/Client.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Client where
 
+import Control.Monad.Trans.Reader
 import Data.Proxy
-import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
 import Servant.API
 import Servant.Client
+import System.Environment (getEnv)
 
 import Competition
 import Team
@@ -15,30 +20,38 @@ import Fixture
 import CompetitionFixture
 import CompetitionTable
 
+type Token = String
 
 type API =
        "competitions"
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] [Competition]
   :<|> "competitions"
        :> Capture "id" Int
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] Competition
   :<|> "competitions"
        :> Capture "competition_id" Int
        :> "fixtures"
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] CompetitionFixtures
   :<|> "competitions"
        :> Capture "competition_id" Int
        :> "leagueTable"
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] CompetitionTable
   :<|> "teams"
        :> Capture "id" Int
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] Team
   :<|> "teams"
        :> Capture "team_id" Int
        :> "players"
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] TeamPlayers
   :<|> "fixtures"
        :> Capture "id" Int
+       :> Header "X-Auth-Token" Token
        :> Get '[JSON] Fixture
 
 baseUrl :: BaseUrl
@@ -47,7 +60,7 @@ baseUrl = BaseUrl Http "api.football-data.org" 80 "/v1"
 footballDataApi :: Proxy API
 footballDataApi = Proxy
 
-getCompetitions :: ClientM [Competition]
+getCompetitions :: Token -> ClientM [Competition]
 getCompetition :: Int -> ClientM Competition
 getCompetitionFixtures :: Int -> ClientM CompetitionFixtures
 getCompetitionTable :: Int -> ClientM CompetitionTable
@@ -63,11 +76,23 @@ getCompetitions
   :<|> getFixture
   = client footballDataApi
 
--- runQuery :: a -> Maybe Int -> IO ()
--- runQuery query id = do
---   m <- newManager defaultManagerSettings
---   res <- runClientM (query id) (ClientEnv m baseUrl)
---   case res of
---     Left err -> putStrLn $ "Error: " ++ show err
---     Right object -> do
---       print object
+type FootballDataClient a = ReaderT Token ClientM a
+
+runClient' :: FootballDataClient a -> Token -> ClientEnv -> IO (Either ServantError a)
+runClient' fcm token = runClientM (runReaderT fcm token)
+
+runClient :: FootballDataClient a -> Token -> Manager -> BaseUrl -> IO (Either ServantError a)
+runClient fcm token manager url = runClient' fcm token (ClientEnv manager url)
+
+runFootClient :: Token -> Manager -> FootballDataClient a -> IO (Either ServantError a)
+runFootClient token manager fcm = runClient fcm token manager baseUrl
+
+exampleQuery :: IO ()
+exampleQuery = do
+  token <- getEnv "FOOTBALL_DATA_TOKEN"
+  m <- newManager defaultManagerSettings
+  res <- runFootClient token m $ do
+    c <- getCompetitions
+    let links = Competition._links $ head c
+    pure $ links
+  print res
