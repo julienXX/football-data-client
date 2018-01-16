ffb536fa6f98fa1b40afaccf8f00d4f828544674
Julien Blanchard <julien@sideburns.eu>
2018-01-16 11:42:10 +0100

    Make queries with runFootClient

--- src/Client.hs
+++ src/Client.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -20,38 +21,29 @@ import Fixture
 import CompetitionFixture
 import CompetitionTable
 
-type Token = String
-
 type API =
        "competitions"
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] [Competition]
   :<|> "competitions"
        :> Capture "id" Int
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] Competition
   :<|> "competitions"
        :> Capture "competition_id" Int
        :> "fixtures"
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] CompetitionFixtures
   :<|> "competitions"
        :> Capture "competition_id" Int
        :> "leagueTable"
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] CompetitionTable
   :<|> "teams"
        :> Capture "id" Int
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] Team
   :<|> "teams"
        :> Capture "team_id" Int
        :> "players"
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] TeamPlayers
   :<|> "fixtures"
        :> Capture "id" Int
-       :> Header "X-Auth-Token" Token
        :> Get '[JSON] Fixture
 
 baseUrl :: BaseUrl
@@ -60,7 +52,7 @@ baseUrl = BaseUrl Http "api.football-data.org" 80 "/v1"
 footballDataApi :: Proxy API
 footballDataApi = Proxy
 
-getCompetitions :: Token -> ClientM [Competition]
+getCompetitions :: ClientM [Competition]
 getCompetition :: Int -> ClientM Competition
 getCompetitionFixtures :: Int -> ClientM CompetitionFixtures
 getCompetitionTable :: Int -> ClientM CompetitionTable
@@ -76,22 +68,21 @@ getCompetitions
   :<|> getFixture
   = client footballDataApi
 
-type FootballDataClient a = ReaderT Token ClientM a
+type FootballDataClient a = ClientM a
 
-runClient' :: FootballDataClient a -> Token -> ClientEnv -> IO (Either ServantError a)
-runClient' fcm token = runClientM (runReaderT fcm token)
+runClient' :: FootballDataClient a -> ClientEnv -> IO (Either ServantError a)
+runClient' fcm = runClientM fcm
 
-runClient :: FootballDataClient a -> Token -> Manager -> BaseUrl -> IO (Either ServantError a)
-runClient fcm token manager url = runClient' fcm token (ClientEnv manager url)
+runClient :: FootballDataClient a -> Manager -> BaseUrl -> IO (Either ServantError a)
+runClient fcm manager url = runClient' fcm (ClientEnv manager url)
 
-runFootClient :: Token -> Manager -> FootballDataClient a -> IO (Either ServantError a)
-runFootClient token manager fcm = runClient fcm token manager baseUrl
+runFootClient :: Manager -> FootballDataClient a -> IO (Either ServantError a)
+runFootClient manager fcm = runClient fcm manager baseUrl
 
 exampleQuery :: IO ()
 exampleQuery = do
-  token <- getEnv "FOOTBALL_DATA_TOKEN"
   m <- newManager defaultManagerSettings
-  res <- runFootClient token m $ do
+  res <- runFootClient m $ do
     c <- getCompetitions
     let links = Competition._links $ head c
     pure $ links
